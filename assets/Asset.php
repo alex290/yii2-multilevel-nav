174742fe0b4af99f86496ccf50baf1fdb0acefbe
Alexandr <alex@webgoal.ru>
2023-03-14 15:42:16 +0300

    Update

---
+++ assets/Asset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Yiister
+ * @license https://github.com/yiister/yii2-gentelella/blob/master/LICENSE
+ * @link http://gentelella.yiister.ru
+ */
+
+namespace alex290\multinav\assets;
+
+class Asset extends \yii\web\AssetBundle
+{
+    public $css = [
+        'css/app.css',
+    ];
+
+    public $js = [
+        'js/app.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapPluginAsset',
+    ];
+
+    public $sourcePath = '@alex290/multinav/assets/scr';
+
+}
